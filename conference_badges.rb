3f07d1298b6730ccb2018daab4966fd54512ee5b
Junaid Malik <jmalik981@gmail.com>
2020-04-16 03:06:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,4 @@
 names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
-  name.each do |x|
-    puts "Hello, my name is #{x}"
-  end
+    puts "Hello, my name is #{name}"
 end
